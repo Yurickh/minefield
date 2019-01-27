87e24955ec6764da3d7c8701981df9e8a9dffd5e
Yurick <yurick.hauschild@gmail.com>
2019-01-27 15:06:25 -0200

    ♻️  Use compose

--- src/utils/cells-around.js
+++ src/utils/cells-around.js
@@ -1,3 +1,5 @@
+const c = (f1, f2) => (...params) => f1(f2(...params))
+
 export default function cellsAround({ numCols, numRows }) {
   const up = pos => ({ ...pos, row: pos.row - 1 })
   const down = pos => ({ ...pos, row: pos.row + 1 })
@@ -13,15 +15,16 @@ export default function cellsAround({ numCols, numRows }) {
     }
 
     return [
-      up(left(position)),
-      up(position),
-      up(right(position)),
-      left(position),
-      right(position),
-      down(left(position)),
-      down(position),
-      down(right(position)),
+      c(up, left),
+      up,
+      c(up, right),
+      left,
+      right,
+      c(down, left),
+      down,
+      c(down, right),
     ]
+      .map(move => move(position))
       .filter(
         ({ col, row }) =>
           !outOfBounds(numCols, col) && !outOfBounds(numRows, row)
